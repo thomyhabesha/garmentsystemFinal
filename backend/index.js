6234964b83a06b89f94ee78fd82453724f1b9912
thomas <tafethomas07@gmail.com>
2025-02-12 09:02:00 +0300

    fixed errors2

--- backend/index.js
+++ backend/index.js
@@ -78,5 +78,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
